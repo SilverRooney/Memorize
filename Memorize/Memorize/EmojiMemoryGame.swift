2ea9401b23cfa491489e8882ebdbacdabcbc6ebf
SilverRooney <101233555+SilverRooney@users.noreply.github.com>
2022-04-05 12:19:13 -0500

    updating files

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Silver on 3/14/22.
+//
+
+import SwiftUI
+
+
+class EmojiMemoryGame: ObservableObject {
+    
+    static let emojis = ["🦇","😸","😈","☠️","🍎","🍭","🙀","😱","🎃","🕸","🐺","🦉","🌛"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in emojis[pairIndex]}
+    }
+    
+   @Published private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+         model.cards
+    }
+    
+    
+    // MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
